3687a7e12bd4b1f4e86a8e5012e80f2dc3756878
小猪佩啥 <sunhao@shouqianba.com>
2022-08-18 23:09:55 +0800

    commits

---
+++ com.xiaozhupeisha.codes/LeetCode0001.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ */
+public class LeetCode0001 {
+
+  /**
+   * 我们可以先对 nums 排序，然后利用的左右双指针技巧，从两端相向而行就行了：
+   *
+   * @param nums
+   * @param target
+   * @return
+   */
+  public int[] twoSum(int[] nums, int target) {
+    // 边界值
+    if (nums.length == 2) {
+      return new int[]{0, 1};
+    }
+
+    // 用来排序的数组
+    int[] numsBrother = Arrays.copyOf(nums, nums.length);
+
+    // 数组排序
+    Arrays.sort(numsBrother);
+
+    // 定义左右指针下坐标
+    int leftIndex = 0, rightIndex = numsBrother.length - 1;
+
+    // 开始穷举
+    while (leftIndex < rightIndex) {
+      int addResult = numsBrother[leftIndex] + numsBrother[rightIndex];
+      if (addResult < target) {
+        leftIndex++;
+      }
+      if (addResult > target) {
+        rightIndex--;
+      }
+      if (addResult == target) {
+        break;
+      }
+    }
+
+    // 定义正确的左右指针下坐标
+    int leftRealIndex = -1, rightRealIndex = -1;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == numsBrother[leftIndex] && leftRealIndex == -1) {
+        leftRealIndex = i;
+        continue;
+      }
+      if (nums[i] == numsBrother[rightIndex] && rightRealIndex == -1) {
+        rightRealIndex = i;
+      }
+    }
+
+    return new int[]{leftRealIndex, rightRealIndex};
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[]{3, 2, 3};
+    int target = 6;
+
+    int[] ints = new LeetCode0001().twoSum(nums, target);
+    for (int anInt : ints) {
+      System.out.println(anInt);
+    }
+  }
+}
---
+++ com.xiaozhupeisha.codes/LeetCode0002.java
@@ -0,0 +1,67 @@
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ */
+public class LeetCode0002 {
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    // 拿到2个链表的初始化指针
+    ListNode p1 = l1, p2 = l2;
+
+    // 初始化节点的头，防止节点错乱
+    ListNode dummy = new ListNode(-1);
+
+    // 新节点的指针，由它来构建新的链表，最终返回的则是初始化的dummy
+    ListNode p = dummy;
+
+    // 用来记录每次循环生成的进位，0或者1
+    int carry = 0;
+    while (p1 != null || p2 != null || carry > 0) {
+      // 本次2个链表节点的相加结果，首先应该等于上次的进位，因为进位不能丢掉
+      int addResult = carry;
+
+      if (p1 != null) {
+        // 本次2个链表节点的相加结果，处于 0-18 之间
+        addResult += p1.val;
+        p1 = p1.next;
+      }
+      if (p2 != null) {
+        // 本次2个链表节点的相加结果，处于 0-18 之间
+        addResult += p2.val;
+        p2 = p2.next;
+      }
+      // 计算出本次的进位，其实就是➗10算整数
+      carry = addResult / 10;
+      // 计算出本次的值，其实就是➗10算余数
+      addResult = addResult % 10;
+
+      // 相加结束后，构建新的链表的本个节点
+      p.next = new ListNode(addResult);
+      // 移动指针到新的节点，老的节点以及在dummy里面了
+      p = p.next;
+    }
+    return dummy.next;
+  }
+
+
+  public static void main(String[] args) {
+
+  }
+}
