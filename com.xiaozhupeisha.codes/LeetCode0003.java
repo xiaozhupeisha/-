8cda6a69a58270b3a3b76699cf4a4ffbfb7ee7cf
小猪佩啥 <sunhao@shouqianba.com>
2022-08-20 11:13:19 +0800

    commits

--- com.xiaozhupeisha.codes/LeetCode0003.java
+++ com.xiaozhupeisha.codes/LeetCode0003.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 
 /**
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -16,25 +14,40 @@ public class LeetCode0003 {
     LinkedHashMap<Character, Integer> characters = new LinkedHashMap<>();
     // 定义窗口左右的指针
     int left = 0, right = 0;
-    // 更新滑动窗口右侧的值
-    Character rightNewestValue = null;
+    // 定义窗口最大的情况
+    int maxLength = 0;
     // 右指针向右移动，一直到边界
     while (right < s.length()){
       // 当前右指针移动到的位置所对应的值
       char nowValue = s.charAt(right);
+      // 更新LinkedHashMap的值，用于下面判断或者移动左边的指针
+      if(!characters.containsKey(nowValue)){
+        characters.put(nowValue, 1);
+      }else{
+        characters.put(nowValue, characters.get(nowValue) + 1);
+      }
+      // 如果当前窗口内，包含的该nowValue值，则收缩左边界
+      while(characters.get(nowValue) > 1){
+        // 如果左边指针移动经过了当前重复的值，则修改LinkedHashMap里面该重复值数量
+        if(s.charAt(left) == nowValue){
+          characters.put(nowValue, characters.get(nowValue) - 1);
+        }else{
+          characters.put(s.charAt(left), characters.get(s.charAt(left)) - 1);
+        }
+        // 左边指针右移
+        left++;
+      }
       // 拿到值之后移动右指针
       right ++;
-      // 更新滑动窗口右侧的值
-      rightNewestValue = nowValue;
-      // 更新LinkedHashMap的值，用于下面判断或者移动左边的指针
-      // 如果当前窗口内，包含的该nowValue值得数量>1，则收缩左边界
-      while(characters.)
+      maxLength = Math.max(maxLength, right - left);
     }
-
-
+    System.out.println("左边指针位置" + left);
+    System.out.println("右边指针位置" + right);
+    return maxLength;
   }
 
   public static void main(String[] args) {
-
+    int i = new LeetCode0003().lengthOfLongestSubstring("tmmzuxt");
+    System.out.println(i);
   }
 }
