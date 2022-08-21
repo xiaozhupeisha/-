a85306b8dd41f6a6ff6b5e9398631a668149ac14
小猪佩啥 <sunhao@shouqianba.com>
2022-08-21 16:54:09 +0800

    commits

---
+++ com.xiaozhupeisha.codes/LeetCode0013.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+
+/**
+ * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做 XXVII, 即为 XX + V + II 。
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给你一个罗马数字，将其转为整数。
+ */
+public class LeetCode0013 {
+
+  public int romanToInt(String roman) {
+    int[] values = {1000, 500, 100, 50, 10, 5, 1};
+    LinkedList<String> symbols = new LinkedList<>();
+    symbols.add("M");
+    symbols.add("D");
+    symbols.add("C");
+    symbols.add("L");
+    symbols.add("X");
+    symbols.add("V");
+    symbols.add("I");
+
+    int intValue = 0;
+    char[] chars = roman.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      char romanChar = roman.charAt(i);
+      // 当前字母对应的罗马字符的值
+      int index = symbols.indexOf(String.valueOf(romanChar));
+      // 提前看一下下一位，如果下一位的索引值比现在的小，则代表要做减法
+      if (i <= chars.length - 2) {
+        char nextRomanChar = roman.charAt(i + 1);
+        int nextCharIndex = symbols.indexOf(String.valueOf(nextRomanChar));
+        int thisCharIndex = symbols.indexOf(String.valueOf(romanChar));
+        if (nextCharIndex < thisCharIndex) {
+          intValue += values[nextCharIndex];
+          intValue -= values[thisCharIndex];
+          i++;
+        } else {
+          // 加值
+          intValue += values[index];
+        }
+      } else {
+        // 加值
+        intValue += values[index];
+      }
+    }
+    return intValue;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new LeetCode0013().romanToInt("MCMXCIV"));
+  }
+}
