29f049cb741dc237eee48183a7d7375761a57cee
小猪佩啥 <sunhao@shouqianba.com>
2022-08-24 17:01:04 +0800

    小猪佩啥的学习记录

--- com.xiaozhupeisha.codes/LeeCode0001.java
+++ com.xiaozhupeisha.codes/LeeCode0001.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  */
-public class LeetCode0001 {
+public class LeeCode0001 {
 
   /**
    * 我们可以先对 nums 排序，然后利用的左右双指针技巧，从两端相向而行就行了：
@@ -61,7 +61,7 @@ public class LeetCode0001 {
     int[] nums = new int[]{3, 2, 3};
     int target = 6;
 
-    int[] ints = new LeetCode0001().twoSum(nums, target);
+    int[] ints = new LeeCode0001().twoSum(nums, target);
     for (int anInt : ints) {
       System.out.println(anInt);
     }
