79f7d56c6d72d0558e2d6261b1edff939e333f47
小猪佩啥 <sunhao@shouqianba.com>
2022-08-24 21:47:18 +0800

    x

--- com.xiaozhupeisha.codes/LeeCode0017.java
+++ com.xiaozhupeisha.codes/LeeCode0017.java
@@ -7,49 +7,50 @@ import java.util.stream.Collectors;
 public class LeeCode0017 {
 
   private String letterMap[] = {
-          " ",    //0
-          "",     //1
-          "abc",  //2
-          "def",  //3
-          "ghi",  //4
-          "jkl",  //5
-          "mno",  //6
-          "pqrs", //7
-          "tuv",  //8
-          "wxyz"  //9
+          " ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"  //9
   };
 
   private ArrayList<String> res;
 
+  /**
+   * 解决一个回溯问题，实际上就是一个决策树的遍历过程，站在回溯树的一个节点上，你只需要思考 3 个问题：
+   * 1、路径：也就是已经做出的选择。
+   * 2、选择列表：也就是你当前可以做的选择。
+   * 3、结束条件：也就是到达决策树底层，无法再做选择的条件。
+   */
   public List<String> letterCombinations(String digits) {
 
-    res = new ArrayList<String>();
-    if (digits.equals(""))
-      return res;
+    res = new ArrayList<>();
+    if (digits.equals("")) return res;
 
+    // 通过递归，实现回溯算法
     findCombination(digits, 0, "");
     return res;
   }
 
-  private void findCombination(String digits, int index, String s) {
-
-    // 2 == 23.length
+  /**
+   * @param digits:   23,34,2~9 两两组合的
+   * @param index:    路径的长度，在这里就1或者2
+   * @param indexStr: 路径的值，比如"a"，"ad"
+   */
+  private void findCombination(String digits, int index, String indexStr) {
+    // 如果路径长度到达了目标的值，比如说在这里就是2。则加入当前路径生成的值
     if (index == digits.length()) {
-      res.add(s);// "ad"
+      System.out.println("达到目标值,digits=" + digits + ",index=" + index + ",indexStr=" + indexStr);
+      res.add(indexStr);
       return;
     }
-
-    char c = digits.charAt(index);// 2 -> 3
-    String letters = letterMap[c - '0'];// abc -> def
-    for (int i = 0; i < letters.length(); i++) {
-      // findCombination(23, 1, "" + a);
-      //
-      // findCombination(23, 2, a + d);
-      // return ad
-      findCombination(digits, index + 1, s + letters.charAt(i));
+    // 如果路径没走到底，则根据树的根节点，进行递归
+    char c = digits.charAt(index);
+    System.out.println("当前节点digits" + c);
+    String letter = letterMap[c - '0'];
+    System.out.println("当前节点所有值" + letter);
+    for (int i = 0; i < letter.length(); i++) {
+      System.out.println("开始回溯,digits=" + digits + ",index=" + (index + 1) + ",indexStr=" + indexStr + letter.charAt(i));
+      findCombination(digits, index + 1, indexStr + letter.charAt(i));
+      System.out.println("============================================");
     }
 
-    return;
   }
 
   public static void main(String[] args) {
