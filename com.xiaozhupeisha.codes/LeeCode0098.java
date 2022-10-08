2012b62300325c87e7608ae153c420a42d20b598
小猪佩啥 <sunhao@shouqianba.com>
2022-10-08 10:15:33 +0800

    小猪佩啥的学习记录

---
+++ com.xiaozhupeisha.codes/LeeCode0098.java
@@ -0,0 +1,38 @@
+/**
+ * 给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
+ */
+public class LeeCode0098 {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public boolean isValidBST(TreeNode root) {
+    return isValidBST(root, null, null);
+  }
+
+  public boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+    if (root == null) {
+      return true;
+    }
+    if (min != null && root.val <= min.val) return false;
+    if (max != null && root.val >= max.val) return false;
+    // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+  }
+
+}
---
+++ com.xiaozhupeisha.codes/LeeCode0099.java
@@ -0,0 +1,29 @@
+/**
+ * 给你二叉搜索树的根节点 root ，该树中的 恰好 两个节点的值被错误地交换。请在不改变其结构的情况下，恢复这棵树 。
+ */
+public class LeeCode0099 {
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public void recoverTree(TreeNode root) {
+
+  }
+
+
+}
